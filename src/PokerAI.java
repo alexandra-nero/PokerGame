7af28c8f8562d002ac32ec4551da2cd13138ec5b
alexandra-nero <alex@nonasoftware.org>
2016-07-20 10:46:59 -0400

    ai changes

--- src/PokerAI.java
+++ src/PokerAI.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,15 +12,40 @@
  * @author Alexandra
  */
 public class PokerAI {
-    private double cockiness;
-    private int opponentsLastBet;
-    private Card[] currentHand;
-    private boolean beforeDiscard;
-    private int currentRank;
+    private int bluff;
     
-    //public double betterHand(Card[] cards){
-        
-    //}
+    public PokerAI(){
+        Random rand = new Random();
+        bluff = rand.nextInt(10);
+    }
+    
+    public Card[] highCardDiscard(Hand currHand){
+        Card[] disCards = currHand.getCards();
+        if (bluff < 6){
+            return new Card[]{disCards[0], disCards[1], disCards[2]};
+        }
+        else if (bluff < 9){
+            return new Card[]{disCards[0], disCards[1]};
+        }
+        else{
+            return new Card[]{disCards[1]};
+        }
+    }
+    
+    public Card[] onePairDiscard(Hand currHand){
+        Card[] disCards = currHand.getCards();
+        if (bluff )
+    }
+    
+    
+    public Card[] chooseDiscard(Hand currHand){
+        switch(currHand.getRank()){
+            case 0:
+                return highCardDiscard(currHand);
+            case 1:
+                return onePairDiscard(currHand);
+        }
+    }
     
     
 }
